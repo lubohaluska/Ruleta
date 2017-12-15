4503389be193948feb24a7fae03f3621a860593d
CSharpFri <CSharpFri@CLASS-2-06>
2017-12-15 19:05:11 +0100

    upgrade

--- Ruleta/HraciPole.cs
+++ Ruleta/HraciPole.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace Ruleta
 {
-    class HraciPole
+     struct HraciPole
     {
         public int Cislo;
         public Barva Barva;
--- Ruleta/HraciStul.cs
+++ Ruleta/HraciStul.cs
@@ -8,9 +8,34 @@ namespace Ruleta
 {
     class HraciStul
     {
-// hraci stol ma 36 cisiel a nulu
+// hraci stol ma 36 cisiel a nulu a tu sa zaklada
         public HraciPole[] HraciPolicka = new HraciPole[37];
+        public HraciStul()
+        {
+            Barva[] rozmisteniBarev = new Barva[37] {Barva.zelena, Barva.cervena, Barva.cerna, Barva.cervena, Barva.cerna, Barva.cervena, Barva.cerna, Barva.cervena, Barva.cerna, Barva.cervena, Barva.cerna, Barva.cervena, Barva.cerna, Barva.cervena, Barva.cerna, Barva.cervena, Barva.cerna, Barva.cervena, Barva.cerna, Barva.cervena, Barva.cerna, Barva.cervena, Barva.cerna, Barva.cervena, Barva.cerna, Barva.cervena, Barva.cerna, Barva.cervena, Barva.cerna, Barva.cervena, Barva.cerna, Barva.cervena, Barva.cerna, Barva.cervena, Barva.cerna, Barva.cervena, Barva.cerna };
+            for (int i = 0; i < rozmisteniBarev.Length; i++)
+            {
+                HraciPolicka[i].Barva = rozmisteniBarev[i];
+                if (i == 0)
+                {
+                    HraciPolicka[i] = Rada.zadna;
+                    HraciPolicka[i] = Lichost.zadna;
 
-            array HraciStul[Cislo, ]
+                }
+                else
+                {
+                    HraciPolicka[i].Cislo = i;
+                    HraciPolicka[i].Rada = vratRadu(i);
+                    HraciPolicka[i].Lichost = vratLichost(i);
+                }
+
+                private vratLichost(int i)
+                {
+
+                }
+    }
+        }
+        
+        
     }
 }
