6e50239049518e599b7e4962464267f731f96693
CSharpFri <CSharpFri@CLASS-2-06>
2017-12-15 17:55:50 +0100

    Add project files.

---
+++ Ruleta/HraciStul.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ruleta
+{
+    class HraciStul
+    {
+// hraci stol ma 36 cisiel a nulu
+        public HraciPole[] HraciPolicka = new HraciPole[37];
+
+            array HraciStul[Cislo, ]
+    }
+}
---
+++ Ruleta/VlastnostiPolicka.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ruleta
+{
+    public enum Barva
+    {
+        zelena,
+        cerna,
+        cervena
+    }
+
+    enum Lichost
+    {
+        zadna, 
+        suda,
+        licha
+    }
+    enum Rada
+    {
+        zadna,
+        prvni,
+        druha,
+        treti
+    }
+    enum Rozsah
+    {
+        zadny,
+        R1_12,
+        R13_24,
+        R25_36,
+        R1_18,
+        R19_36
+    }
+
+}
